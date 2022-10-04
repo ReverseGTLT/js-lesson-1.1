a53b5584f8c5e534c544a37f1effdc53c61b8a34
ReverseGTLT <108753232+ReverseGTLT@users.noreply.github.com>
2022-10-04 21:39:50 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,3 @@
-var name = prompt("Iм'я користувача", '');
-var result = 'Hello, ' + name + '! How are you?';
+const name = prompt("Iм'я користувача", '');
+const result = 'Hello, ' + name + '! How are you?';
 alert(result);
